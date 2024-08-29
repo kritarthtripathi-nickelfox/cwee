58181deca45be5f8a4fe59fb315cc8b9be8168cd
Sonika Soni <sonikasoni@pop-os.localdomain>
2024-08-29 16:50:04 +0530

    css change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Fade from "react-reveal/Fade";
-import Slide from "react-reveal/Slide";
-import Zoom from "react-reveal/Zoom";
 import FirstScreen from "./FirstScreen";
 import DesertScreen from "./DesertScreen";
 import FirstScreenContentPart from "./FirstScreenContentPart";
