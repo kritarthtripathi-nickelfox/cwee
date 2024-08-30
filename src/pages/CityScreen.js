0f9aea16ff7edcba822d6df41aee261fc8e425b7
Sonika Soni <sonikasoni@pop-os.localdomain>
2024-08-30 16:55:04 +0530

    transition

--- src/pages/CityScreen.js
+++ src/pages/CityScreen.js
@@ -9,7 +9,10 @@ function CityScreen() {
     const [state, setState] = useState({
         isPaneOpenRight: false,
         isPaneOpenLeft: false,
+        isPaneOpenLeftBottomTamil:false,
+        isPaneOpenLeftBottomAndhra:false
     });
+    const [position, setPosition] = useState(null)
 
 
     useEffect(() => {
@@ -19,14 +22,26 @@ function CityScreen() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, [scrollY]);
 
+    const handlePaneBottomClickTamil = () => {
+        setState({ isPaneOpenLeftBottomTamil: true })
+        setScrollY(0)
+        setPosition('bottom')
+    }
+    const handlePaneBottomClickAndhra = () => {
+        setState({ isPaneOpenLeftBottomAndhra: true })
+        setScrollY(0)
+        setPosition('bottom')
+    }
     const handlePaneLeftClick = () => {
         setState({ isPaneOpenLeft: true })
         setScrollY(0)
+        setPosition('left')
+
     }
-
-
     const handlePaneRightClick = () => {
         setState({ isPaneOpenRight: true })
+        setScrollY(0)
+        setPosition('right')
     }
 
 
@@ -41,13 +56,13 @@ function CityScreen() {
                 <div className='buttonContainer'>
                     <button
                         className="CityOneButton"
-                        onClick={handlePaneLeftClick}
+                        onClick={handlePaneBottomClickTamil}
                     >
                         Explore TamilNadu
                     </button>
                     <button
                         className="CityTwoButton"
-                        onClick={handlePaneRightClick}
+                        onClick={handlePaneBottomClickAndhra}
                     >
                         Explore AndhraPradesh
                     </button>
@@ -56,14 +71,14 @@ function CityScreen() {
             <SlidingPane
                 className="SlidePane"
                 overlayClassName="SlidePane"
-                isOpen={state.isPaneOpenLeft}
+                isOpen={state.isPaneOpenLeftBottomTamil ?? state.isPaneOpenLeft}
                 title=""
                 subtitle=""
                 hideHeader
-                from="bottom"
+                from={position}
                 onRequestClose={() => {
                     // triggered on "<" on left top click or on outside click
-                    setState({ isPaneOpenLeft: false });
+                    setState({ isPaneOpenLeftBottomTamil: false });
                 }}
             >
                 <div className="relative min-h-screen">
@@ -93,7 +108,7 @@ function CityScreen() {
                             <div className='buttonContainer'>
                                 <button
                                     className="CityOneButton"
-                                onClick={handlePaneRightClick}
+                                    onClick={handlePaneRightClick}
                                 >
                                     Explore AndhraPradesh
                                 </button>
@@ -111,29 +126,60 @@ function CityScreen() {
             <SlidingPane
                 className="SlidePane"
                 overlayClassName="SlidePane"
-                isOpen={state.isPaneOpenRight}
+                isOpen={state.isPaneOpenLeftBottomAndhra ?? state.isPaneOpenRight}
                 title=""
                 subtitle=""
                 hideHeader
-                from="bottom"
+                from={position}
 
                 onRequestClose={() => {
                     // triggered on "<" on left top click or on outside click
-                    setState({ isPaneOpenRight: false });
+                    setState({ isPaneOpenLeftBottomAndhra: false });
                 }}
             >
-                <div className='AndhraPradeshScreen'>
+                <div className="relative min-h-screen">
                     <div
-                        className='absolute left-[calc(50%_-_400px)] rounded-3xs bg-gray-100 flex flex-row items-center justify-center p-[2.5rem] box-border Content'
-                    >
-                        <div className=" w-[56.563rem] relative tracking-[-0.02em] leading-[3.5rem] text-[2rem]  items-center shrink-0">
-                            Tamil Nadu offers more than 150 GW of low-cost wind potential. Up to 50 GW, it has the lowest wind power generation costs. However, 23% of its ~500 GW solar potential is in districts such as Tenkasi and Coimbatore, with high land prices.
+                        className="fixed inset-0 bg-cover bg-center AndhraPradeshScreenFade"
+                    />
+                    {scrollY < 300 ? (
+
+                        <div className='AndhraPradeshScreen'>
+                            <div
+                                className='absolute left-[calc(50%_-_400px)] rounded-3xs bg-gray-100 flex flex-row items-center justify-center p-[2.5rem] box-border Content'
+                            >
+                                <div className=" w-[56.563rem] relative tracking-[-0.02em] leading-[3.5rem] text-[2rem]  items-center shrink-0">
+                                    With a solar potential of 233 GW and wind potential
+                                    of 16 GW, Chittoor in Andhra Pradesh is a great site for
+                                    clean energy development. However, the district—a 4-hour
+                                    drive from Chennai and Bengaluru— has a high population
+                                    density of 350+ people/square kilometre which might mean us running into problems of resident reluctance, complex pre-existing infrastructure, and unavailability of large continuous chunks of land.                          </div>
+                            </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div
+                            className='absolute left-[calc(50%_-_400px)] rounded-3xs bg-gray-100 flex items-center justify-center p-[2.5rem] box-border Content'
+                        >
+                            <div className=" w-[56.563rem] relative tracking-[-0.02em] leading-[3.5rem] text-[2rem]  items-center shrink-0">
+                                Scaling RE beyond 300 GW is where things could start getting more complicated. Bustling cities and areas with steeper land prices and land conflicts enter the mix. What does that look like, you ask? Let’s explore.
+                            </div>
+                            <div className='buttonContainer'>
+                                <button
+                                    className="CityOneButton"
+                                    onClick={handlePaneLeftClick}
+                                >
+                                    Explore TamilNadu
+                                </button>
+                                <button
+                                    className="CityTwoButton"
+                                // onClick={handlePaneRightClick}
+                                >
+                                    Move Ahead
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
-                {/* <div>And I am pane content. BTW, what rocks?</div> */}
                 <br />
-                {/* <img src="img.png" /> */}
             </SlidingPane>
         </div>
     );
